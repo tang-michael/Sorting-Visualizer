be6d1b01649ec411a215e3d236a66d512c303260
tang-michael <43308145+tang-michael@users.noreply.github.com>
2020-06-02 17:11:28 -0400

    Update InsertionSort.java

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -18,21 +18,17 @@ public class InsertionSort {
 	public void sort() {
 		int n = arr.length;
 
-		// One by one move boundary of unsorted subarray
 		for (int i = 0; i < n - 1; i++) {
-			// Find the minimum element in unsorted array
 			int min_idx = i;
 			for (int j = i + 1; j < n; j++)
 				if (arr[j] < arr[min_idx])
 					min_idx = j;
 
-			// Swap the found minimum element with the first
-			// element
 			int temp = arr[min_idx];
 			arr[min_idx] = arr[i];
 			arr[i] = temp;
 			try {
-				Thread.sleep(100); // Sleeps so update isn't "instant"
+				Thread.sleep(100); 
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
